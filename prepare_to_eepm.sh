ea11c6a50d4260a0fd00e8dca6e525125df11400
Vitaly Lipatov <lav@etersoft.ru>
2026-01-13 01:35:45 +0300

    prepare_to_eepm.sh: fix

--- prepare_to_eepm.sh
+++ prepare_to_eepm.sh
@@ -1,3 +1,3 @@
 
-subst 's|a/eget|a/bin/tools_eget|' 000*.patch
-subst 's|b/eget|b/bin/tools_eget|' 000*.patch
+subst 's|a/eget|a/bin/tools_eget|' 00**.patch
+subst 's|b/eget|b/bin/tools_eget|' 00**.patch
